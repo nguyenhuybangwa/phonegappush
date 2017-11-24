d5eb16c14da9e70d5001ef3998556c23f91a4629
vp7 <nguyenhuybangwa@gmail.com>
2017-11-25 00:12:23 +0700

    add them

---
+++ www/js/my-app.js
@@ -0,0 +1,93 @@
+// Initialize app
+var myApp = new Framework7();
+
+
+// If we need to use custom DOM library, let's save it to $$ variable:
+var $$ = Dom7;
+
+// Add view
+var mainView = myApp.addView('.view-main', {
+    // Because we want to use dynamic navbar, we need to enable it for this view:
+    dynamicNavbar: true
+});
+
+// Handle Cordova Device Ready Event
+$$(document).on('deviceready', function() {
+    console.log("Device is ready!");
+});
+
+
+// Now we need to run the code that will be executed only for About page.
+
+// Option 1. Using page callback for page (for "about" page in this case) (recommended way):
+myApp.onPageInit('about', function (page) {
+    // Do something here for "about" page
+
+})
+
+// Option 2. Using one 'pageInit' event handler for all pages:
+$$(document).on('pageInit', function (e) {
+    // Get page data from event data
+    var page = e.detail.page;
+
+    if (page.name === 'about') {
+        // Following code will be executed for page with data-page attribute equal to "about"
+        myApp.alert('Here comes About page');
+    }
+})
+
+// Option 2. Using live 'pageInit' event handlers for each page
+$$(document).on('pageInit', '.page[data-page="about"]', function (e) {
+    // Following code will be executed for page with data-page attribute equal to "about"
+    myApp.alert('Here comes About page');
+})
+
+var resultArr = [];
+
+function pushFcm() {
+    const push = PushNotification.init({
+        android: {
+        },
+        browser: {
+            pushServiceURL: 'http://push.api.phonegap.com/v1/push'
+        },
+        ios: {
+            alert: "true",
+            badge: "true",
+            sound: "true"
+        },
+        windows: {}
+    });
+
+    push.on('registration', (data) => {
+        alert('registration');
+        alert(data.registrationId);
+        
+        resultArr.push('<li>registration</li>');
+        resultArr.push('<li>${data.registrationId}</li>');
+        showResult();
+        // data.registrationId
+    });
+
+    push.on('notification', (data) => {
+        resultArr.push('<li>notification</li>');
+        showResult();
+
+        // data.message,
+        // data.title,
+        // data.count,
+        // data.sound,
+        // data.image,
+        // data.additionalData
+    });
+
+    push.on('error', (e) => {
+        resultArr.push('<li>error</li>');
+        showResult();
+        // e.message
+    });
+}
+
+function showResult() {
+    document.getElementById('result').innerHTML = resultArr.join('');
+}
